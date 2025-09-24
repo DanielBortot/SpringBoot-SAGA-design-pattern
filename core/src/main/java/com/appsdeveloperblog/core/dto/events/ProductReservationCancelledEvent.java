9eb05473ba1e41fe7521af08cb18f607046beba9
Daniel Bortot <daniel.bortot@gmail.com>
2025-09-24 16:22:49 -0400

    feat: :tada: SAGA design pattern

---
+++ core/src/main/java/com/appsdeveloperblog/core/dto/events/ProductReservationCancelledEvent.java
@@ -0,0 +1,32 @@
+package com.appsdeveloperblog.core.dto.events;
+
+import java.util.UUID;
+
+public class ProductReservationCancelledEvent {
+    private UUID orderId;
+    private UUID productId;
+
+    public ProductReservationCancelledEvent() {
+    }
+
+    public ProductReservationCancelledEvent(UUID orderId, UUID productId) {
+        this.orderId = orderId;
+        this.productId = productId;
+    }
+
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(UUID orderId) {
+        this.orderId = orderId;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+}
